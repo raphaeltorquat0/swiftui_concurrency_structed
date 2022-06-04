d9d0fb3363f008ac94b78b3030571aa2a985eaa0
Raphael Torquato <89878688+raphaeltorquat0@users.noreply.github.com>
2022-06-04 18:17:44 -0300

    Initial Commit

---
+++ swiftui_concurrency_structed/swiftui_concurrency_structedApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_concurrency_structedApp.swift
+//  swiftui_concurrency_structed
+//
+//  Created by Raphael Torquato on 04/06/22.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_concurrency_structedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
