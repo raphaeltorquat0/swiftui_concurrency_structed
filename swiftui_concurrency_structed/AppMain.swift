f10c1c706b3231a59a53fdb4194d06419fc5101c
Raphael Torquato <89878688+raphaeltorquat0@users.noreply.github.com>
2022-06-04 19:31:18 -0300

    starting project and first commit
    starting project and first commit

---
+++ swiftui_concurrency_structed/AppMain.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+import OSLog
+
+let log = Logger()
+
+@main
+struct AppMain: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
